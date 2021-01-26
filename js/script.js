990010582dfaeea5c75bdd94d1152c55b5bafc8a
Federico <crudofederico@gmail.com>
2021-01-26 20:22:20 +0100

    miss push

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-
 var app=new Vue({
   el:".slider-wrapper",
   data:{
@@ -8,21 +7,35 @@ var app=new Vue({
       "img/immagine2.jpg",
       "img/immagine3.jpg",
       "img/immagine4.jpg"
-    ]
-
+    ],
+  animation:''
   },
+  created(){
+    this.animation=setInterval(this.next,3000);
+  },
+ 
+  
   methods:{
+
     next :function() {
+     
       (this.counter==this.img.length-1)? this.counter=0 : this.counter++;
+      
 
   },
   prev:function () {
     (this.counter<=0)? this.counter=this.img.length-1 : this.counter--;
-    console.log(this.counter);
+    
+    
+  },
+ 
+
+  click: function (index) {
+      this.counter=index;
+     clearInterval(this.animation);
+      
   }
-
-
-
   }
 
 });
+
